5e5c764b0568107f06119a44de434967281daa07
NDORERE Elie <220230018+NDORERE-Elie@users.noreply.github.com>
2026-06-18 11:12:51 +0000

    my first pro in python

---
+++ calculator.py
@@ -0,0 +1,25 @@
+operator=input("Enter the operator(+ - * /):")
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))
+
+if operator== "+":
+    result=num1+num2
+    print(round(result))
+
+    
+elif operator== "-":
+    result=num1-num2
+    print(round(result))
+    
+    
+elif operator== "*":
+    result=num1*num2
+    print(round(result))
+    
+    
+elif operator== "/":
+    result=num1/num2
+    print(round(result))
+    
+else:
+    print(f"{operator} Enter the operator please") 
